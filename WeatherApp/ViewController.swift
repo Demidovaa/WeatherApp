01390b413c1ea091d5bc9950271897de0533576a
Anastasia-Demidova <60142958+Anastasia-Demidova@users.noreply.github.com>
2020-07-04 23:13:16 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Анастасия Демидова on 04.07.2020.
+//  Copyright © 2020 AnastasiaDemidova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
