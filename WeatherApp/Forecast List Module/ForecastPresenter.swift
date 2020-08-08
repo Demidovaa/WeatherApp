b35fc92d44c2b23bbdeb0094cafcee50148003d6
Anastasia Demidova <demidova.nastasya99@gmail.com>
2020-08-09 01:00:25 +0300

    Create Forecast View Controller

---
+++ WeatherApp/Forecast List Module/ForecastPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  ForecastPresenter.swift
+//  WeatherApp
+//
+//  Created by Anastasia Demidova on 09.08.2020.
+//  Copyright © 2020 AnastasiaDemidova. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/Forecast List Module/ForecastViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastViewController.swift
+//  WeatherApp
+//
+//  Created by Anastasia Demidova on 09.08.2020.
+//  Copyright © 2020 AnastasiaDemidova. All rights reserved.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
