1e76ad625a8c2f5773b5b2d81d204e1500fb2418
Anastasia Demidova <demidova.nastasya99@gmail.com>
2020-08-29 22:52:17 +0300

    Update UI element

--- WeatherApp/City List Module/CityListViewController.swift
+++ WeatherApp/City List Module/CityListViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 import CoreLocation
 
 class CityListViewController: UIViewController {
-    
     @IBOutlet private weak var tableView: UITableView!
+    
+    var city: WeatherCity?
     
     private var presenter: CityListPresenter?
     private let locationService = GeolocationService()
     
+    final let headerSections = ["Current", "Сities"]
+    private var location = ["Nizhny Novgorod"]
     private var cityArray = ["Nizhny Novgorod", "Moscow"]
     
     override func viewDidLoad() {
@@ -26,37 +29,66 @@ class CityListViewController: UIViewController {
         tableView.dataSource = self
         //MARK: Location
         locationService.start()
+        
     }
 }
 
 //MARK: TableView
 extension CityListViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cityArray.count
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return headerSections[0]
+        } else {
+            return headerSections[1]
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: "CityTableViewCell", for: indexPath)
             as? CityTableViewCell else { return UITableViewCell() }
-        let item = cityArray[indexPath.row]
-        cell.configure(name: item, temp: 21)
+        
+        switch (indexPath.section) {
+        case 0:
+            let item = location[indexPath.row]
+            cell.configure(name: item, temp: 20)
+        case 1:
+            let items = cityArray[indexPath.row]
+            cell.configure(name: items, temp: 21)
+            return cell
+        default:
+            fatalError("NoCell")
+        }
+        
         return cell
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 1 //use one cell for current geolocaton
+        case 1: return 2
+        default:
+            fatalError("NoRows")
+        }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     //MARK: Delete Cell
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         true
     }
-
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
                    forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
