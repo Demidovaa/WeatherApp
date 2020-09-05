375ea0bca2c46255da14851e3effa2db732e4682
Anastasia Demidova <demidova.nastasya99@gmail.com>
2020-09-05 23:46:24 +0300

    Update UI: table view cell

--- WeatherApp/Forecast List Module/ForecastTableViewCell.swift
+++ WeatherApp/Forecast List Module/ForecastTableViewCell.swift
@@ -9,5 +9,14 @@
 import UIKit
 
 class ForecastTableViewCell: UITableViewCell {
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var weatherImage: UIImageView!
+    @IBOutlet private weak var maxTempLabel: UILabel!
+    @IBOutlet private weak var minTempLabel: UILabel!
     
+    func configure(date: String, maxTemp: String, minTemp: String) {
+        dateLabel.text = date
+        maxTempLabel.text = maxTemp
+        minTempLabel.text = minTemp
+    }
 }
--- WeatherApp/Forecast List Module/ForecastViewController.swift
+++ WeatherApp/Forecast List Module/ForecastViewController.swift
@@ -13,8 +13,12 @@ class ForecastViewController: UIViewController {
     @IBOutlet private weak var collectionView: UICollectionView!
     @IBOutlet private weak var tableView: UITableView!
     
+    private var presenter: ForecastPresenter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter = ForecastPresenter()
+        
         tableView.delegate = self
         tableView.dataSource = self
         collectionView.delegate = self
@@ -26,7 +30,7 @@ class ForecastViewController: UIViewController {
 
 extension ForecastViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 10 //test
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -41,13 +45,15 @@ extension ForecastViewController: UICollectionViewDelegate, UICollectionViewData
 
 extension ForecastViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 10 //test
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
         withIdentifier: "ForecastTableViewCell", for: indexPath)
         as? ForecastTableViewCell else { return UITableViewCell() }
+        
+        cell.configure(date: "Monday, 5", maxTemp: "+21", minTemp: "+9")
         
         return cell
     }
